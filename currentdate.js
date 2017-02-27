e097b9665b8b93236bb26c1535f4610508388dc4
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-02-27 23:52:01 +0200

    "Add extract data module and get date&time module and implement the condition"

---
+++ currentdate.js
@@ -0,0 +1,18 @@
+function curentdate(){
+  var currentdate = new Date();
+  var datetime = [];
+   datetime[0] =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/"
+                + currentdate.getFullYear();
+
+   datetime[1] =currentdate.getHours() + ":"
+                + currentdate.getMinutes();
+
+
+   return datetime;
+
+}
+
+module.exports = {
+  currentdate:curentdate
+}
--- extractdata.js
+++ extractdata.js
@@ -1,9 +1,13 @@
+var datetime = require('./currentdate.js');
+
 function extractdata(jsondata){
   jsondata = JSON.parse(jsondata).values;
   jsondata.reduce(function(acc,curr,index){
     curr.reduce(function(inacc,incurr,inindex){
-      console.log(index,curr[inindex]);
-
+      console.log(index,curr[inindex],inindex);
+      if(curr[inindex]==datetime.currentdate()[0]){
+        console.log(curr[inindex]+" = " +datetime.currentdate()[0]);
+      }
 
     },0);
 
