ce9c5d5c60c55aa870cb4065ad1181d8bd8033fd
{m_mushtaha} <{muhamed.mushtaha@outlook.com}>
2017-02-28 16:36:13 +0200

    edit index.js

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ var SHEET_ID = '1DY-JnL0myVggCoFIobL8FNIO3qlGg7mQOi97tzx8M4Q';
 function getdata(callback){
   var jwtClient = new google.auth.JWT(key.client_email,
     null,
-    key.private_key, ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    key.private_key, ['https://www.googleapis.com/auth/spreadsheets'],
     null
   );
 
   jwtClient.authorize((err, tokens) => {
+    console.log(tokens);
     if (err) {
       console.log(err);
       return;
@@ -23,20 +24,27 @@ function getdata(callback){
     var opts = {
       hostname: 'sheets.googleapis.com',
       port: 443,
-      path: `/v4/spreadsheets/${SHEET_ID}/values/mealsheet`,
-      method: 'GET',
+      path: `/v4/spreadsheets/${SHEET_ID}/values:batchUpdate`,
+
+      method: 'POST',
       headers: {
-              'Authorization': `Bearer ${tokens.access_token}`
-      }
+              'Authorization': `Bearer ${tokens.access_token}`}
+
+            payload:{  "range": "A5:D5",
+            "values": [
+              [
+                  ss,sss,ss,ss
+              ],
+
+            ]}
+
     };
 
     var req = https.request(opts, (res) => {
       var store = '';
       res.on('data', (chunk) => store = store + chunk);
       res.on('end', () => {
-      //  var data = accessextractdata.extractdata(store)
         console.log("store",store);
-      //  callback(data);
       });
 
     });
