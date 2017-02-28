e3d7620faf0dad3a04b985caca2f3e80ed0f25eb
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-02-28 08:13:51 -0500

    return the right data

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 'use strict';
+
 var https = require('https');
 var google = require('googleapis');
 var key = require('./key.json');
 var accessextractdata = require('./server/extractdata.js');
+
 var SHEET_ID = '1DY-JnL0myVggCoFIobL8FNIO3qlGg7mQOi97tzx8M4Q';
-var jwtClient = new google.auth.JWT(key.client_email,
+
+function getdata(callback){
+  var jwtClient = new google.auth.JWT(key.client_email,
     null,
     key.private_key, ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-    null);
-jwtClient.authorize((err, tokens) => {
+    null
+  );
 
+  jwtClient.authorize((err, tokens) => {
     if (err) {
-        console.log(err);
-        return;
+      console.log(err);
+      return;
     }
 
-    console.log('token', tokens);
     var opts = {
-        hostname: 'sheets.googleapis.com',
-        port: 443,
-        path: `/v4/spreadsheets/${SHEET_ID}/values/mealsheet`,
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${tokens.access_token}`
-        }
+      hostname: 'sheets.googleapis.com',
+      port: 443,
+      path: `/v4/spreadsheets/${SHEET_ID}/values/mealsheet`,
+      method: 'GET',
+      headers: {
+              'Authorization': `Bearer ${tokens.access_token}`
+      }
     };
-    console.log('here 2');
+
     var req = https.request(opts, (res) => {
-        console.log('here 3');
-        var store = '';
-        res.on('data', (chunk) => store = store + chunk);
-        res.on('end', () => accessextractdata.extractdata(store));
+      var store = '';
+      res.on('data', (chunk) => store = store + chunk);
+      res.on('end', () => {
+      //  var data = accessextractdata.extractdata(store)
+        console.log("store",store);
+      //  callback(data);
+      });
+
     });
+
     req.end();
+  });
+}
+
+getdata(function(ss){
+
+  console.log("ss",ss);
 });
