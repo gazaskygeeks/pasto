7a0ce10c020dc5ecca6c47127185749b02ac1d89
mhmdshorafa <mhmd_shorafa@hotmail.com>
2017-03-02 07:58:50 +0200

    "some added write data files and get data depend on mealcategory"

--- app/utils/get-info.js
+++ app/utils/get-info.js
@@ -1,15 +1,22 @@
 var date = require('./get-date.js')
 
+
 function extractdata(jsondata) {
   var alldata = [];
   var member = [];
   var todayorders = [];
   var names = [];
   var data = jsondata.values;
+  var currenthour = new Date();
+  var mealcategory="breakfast";
+  if(currenthour.getHours()<10){mealcategory = "lunch"}
+  console.log("currenthour.getHours()",currenthour.getHours());
+
   data.forEach(function(elem) {
     for (var i = 0; i < elem.length -1; i++) {
 
-      if (elem[i] == date.currentdate()[0]) {
+      if (elem[i] == date.currentdate()[0]&&elem[1]== mealcategory ) {
+        console.log(i,elem[i]);
         todayorders.push(elem[0]);
         names.push(elem[5]);
       }
