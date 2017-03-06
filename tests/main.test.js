7e64abe6038d959bc8d84b76258a2bbaea20ce55
besart <besartshyti@gmail.com>
2017-03-06 12:39:09 +0200

    move tests to separate files and comment the one not passing

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,16 +1,8 @@
+'use strict';
+
 var test = require('tape');
 var shot = require('shot');
 var mainHandler = require('../main.js');
-var getinfo = require('../app/utils/get-info.js');
-var getdate = require('../app/utils/get-date.js');
-var read = require('../app/utils/read.js');
-var app = require('../app/utils/index.js');
-var mock = require('./mock.js');
-
-var testdatetime = [];
-var testDateTime = new Date();
-testdatetime[0] = testDateTime.getDate() + "/" + (testDateTime.getMonth() + 1) + "/" + testDateTime.getFullYear();
-testdatetime[1] = testDateTime.getHours() + ":" + testDateTime.getMinutes();
 
 test('GET /: should return Error', function(t) {
   shot.inject(mainHandler, {
@@ -51,7 +43,6 @@ test('GET /: should return img.jpg', function(t) {
     method: 'GET',
     url: '/img.jpg'
   }, function(res) {
-    // console.log('res.payload', res.payload)
     var indexOf = res.payload.indexOf('Ǻ6Fp');
     t.equal(indexOf, -1, 'got Ǻ6Fp somewhere in the html');
     t.end();
@@ -101,31 +92,3 @@ test('GET /: should return writedata.js', function(t) {
     t.end();
   });
 });
-
-test("get-date.js should return arry of date and time", function(t) {
-  var time = getdate.currentdate();
-  t.deepEqual(time, testdatetime, "Data is Correct");
-  t.end();
-});
-
-test("extractdata should return the right data", function(t) {
-  var data = getinfo.extractdata(mock);
-  var simulatedata = [
-    ['book'],
-    ['abeer']
-  ]
-  t.deepEqual(data, simulatedata, "Data is Correct");
-  t.end();
-});
-var expcted = ["Email", "abber2000g@gmail.com", "ibraghada@gmail.com", "alaakhattab92@gmail.com", "shahy.m.93@gmail.com", "rana@gazaskygeeks.com", "a.alshaikh82@gmail.com", "Akram.badah@gmail.com", "muhamed.mushtaha@outlook.com", "Moha.buy@gmail.com"]
-test('read.sj should read sheet files', function(t) {
-  shot.inject(mainHandler, {
-    method: 'GET',
-    url: '/readmemberssheet'
-  }, function(res) {
-    var result = res.payload
-    var parseresult = JSON.parse(result)
-    t.deepEqual(parseresult,expcted, 'got member emails');
-    t.end();
-  });
-})
---
+++ tests/runner.js
@@ -0,0 +1,4 @@
+'use strict';
+
+require('./main.test.js');
+require('./utils.test.js');
